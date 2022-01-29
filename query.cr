532be5567cda27aabcf77edcbece3bd48febddc7
Karl Matthias <karl@matthias.org>
2022-01-29 11:56:47 +0000

    Cleanup, refactor, add StringPool

--- query.cr
+++ query.cr
@@ -3,7 +3,9 @@ require "uuid"
 require "json"
 
 class PrestoClient
-  def initialize(@host_port : String, @user : String, @catalog : String, @schema : String); end
+  def initialize(@host_port : String, @user : String, @password : String? = nil)
+    @pool = StringPool.new
+  end
 
   def query(query : String, options : Hash(Symbol, String) = {} of Symbol => String)
     response = initial_request(query)
@@ -12,18 +14,58 @@ class PrestoClient
     next_uri = body["nextUri"]
     state = body["stats"].not_nil!["state"]
 
-    data = [] of Hash(String, String)
+    data = [] of JSON::Any
+    columns = [] of String
     loop do
-      body = follow_up(next_uri, state, options)
+      body = advance(next_uri, state, options)
       data = accumulate(data, body)
-      break if complete?(body)
+      if complete?(body)
+        columns = get_columns(body)
+        break
+      end
       next_uri = body["nextUri"]
     end
 
     if errored?(body)
       {status: "FAILED", error: body["error"]}
     else
-      {status: "OK", data: data.flatten}
+      rows = data.map { |row| columns.zip(row.as_a).to_h }
+      {status: "OK", data: rows}
+    end
+  end
+
+  private def initial_request(query)
+    HTTP::Client.post(
+      "http://#{@host_port}/v1/statement",
+      HTTP::Headers{
+        "X-Presto-User"    => @user,
+        "X-Trino-User"     => @user,
+        "X-Presto-Source"  => "Crystal client",
+        "X-Trino-Source"   => "Crystal client"
+      },
+      query
+    )
+  end
+
+  private def accumulate(acc, body)
+    b = body["data"].as_a
+    acc + b
+  rescue KeyError
+    acc
+  end
+
+  private def get_columns(body)
+    body["columns"].as_a.map { |c| @pool.get(c["name"].as_s) }
+  rescue KeyError
+    [] of String
+  end
+
+  private def advance(url, state, options)
+    if %w{QUEUED RUNNING}.includes?(state)
+      response = HTTP::Client.get(url.to_s)
+      JSON.parse(response.body)
+    else
+      JSON::Any.new(nil)
     end
   end
 
@@ -34,50 +76,16 @@ class PrestoClient
   private def errored?(body)
     body.dig("stats", "state") == "FAILED"
   end
-
-  private def accumulate(acc, body)
-    b = body["data"].as_a
-    acc + b
-  rescue KeyError
-    acc
-  end
-
-  private def initial_request(query)
-    HTTP::Client.post(
-      "http://#{@host_port}/v1/statement",
-      HTTP::Headers{
-        "X-Presto-User"    => @user,
-        "X-Trino-User"     => @user,
-        "X-Presto-Source"  => "Crystal client",
-        "X-Trino-Source"   => "Crystal client",
-        "X-Presto-Catalog" => @catalog,
-        "X-Trino-Catalog"  => @catalog,
-        "X-Presto-Schema"  => @schema,
-        "X-Trino-Schema"   => @schema,
-      },
-      query
-    )
-  end
-
-  private def follow_up(url, state, options)
-    if %w{QUEUED RUNNING}.includes?(state)
-      response = HTTP::Client.get(url.to_s)
-      JSON.parse(response.body)
-    else
-      JSON::Any.new(nil)
-    end
-  end
 end
 
-client = PrestoClient.new("127.0.0.1:8080", "kmatthias", "cassandra", "summaries_test")
+client = PrestoClient.new("127.0.0.1:8080", "kmatthias")
 resp = client.query(<<-EOF
   SELECT bv.client_id, bv.tag_key, bv.value
   FROM cassandra.subscription_data.by_value AS bv
   WHERE bv.client_id = '71d32975-d8df-4906-a1bf-4fa493528532'
     AND bv.tag_key = 'club'
     AND bv.value in ('lions', 'elks')
-  LIMIT 50
 EOF
 )
 
-pp resp
+#p resp
