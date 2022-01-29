e5fcd778239a046287097a92cb3cc92574d561ae
Karl Matthias <karl@matthias.org>
2022-01-29 17:59:58 +0000

    Structure as a proper lib

--- src/trino-client/trino_client.cr
+++ src/trino-client/trino_client.cr
@@ -118,25 +118,3 @@ class TrinoClient
     body.dig("stats", "state") == "FAILED"
   end
 end
-
-require "option_parser"
-
-user = ENV["USER"] || "unknown-user"
-hostname = "localhost"
-port = 8080
-
-OptionParser.parse do |parser|
-  parser.banner = "Usage: query [arguments]"
-  parser.on("-u USERNAME", "--user=USERNAME", "Username to send to Trino ('#{user}')") { |u| user = u }
-  parser.on("-h HOSTNAME", "--host=HOSTNAME", "Hostname/IP to connect to Trino ('#{hostname}')") { |h| hostname = h }
-  parser.on("-p PORT", "--port=PORT", "Port to connect to Trino (#{port})") { |p| post = p }
-  parser.on("--help", "Show this help") { puts parser; exit }
-end
-
-client = TrinoClient.new("#{hostname}:#{port}", user)
-start_time = Time.local
-resp = client.query(STDIN.gets_to_end)
-elapsed = "elapsed: #{Time.local - start_time}"
-
-puts resp.to_pretty_json
-STDERR.puts elapsed
